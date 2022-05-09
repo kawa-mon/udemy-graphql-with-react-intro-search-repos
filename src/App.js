684fbdc155f131a571b468a92d255d45fdb0d8b9
kawa-mon <kawashingo1990@gmail.com>
2022-05-09 11:58:29 +0900

    separate-graphql-code

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import { ApolloProvider } from 'react-apollo'
-import gql from 'graphql-tag'
 import { Query } from 'react-apollo'
 import client from './client'
-
-const ME = gql`
-  query me {
-    user(login: "iteachonudemy") {
-      name
-      avatarUrl
-    }
-  }
-`
+import { ME } from './graphql'
 
 function App() {
   return (
     <ApolloProvider client={client}>
       <div>Hello, GraphQL</div>
       <Query query={ME}>
-        {
-          ({ loading, error, data }) => {
+        {({ loading, error, data }) => {
           if (loading) return 'Loading...'
           if (error) return `Error! ${error.message}`
 
           return <div>{data.user.name}</div>
-          }
-        }
+        }}
       </Query>
     </ApolloProvider>
   )
