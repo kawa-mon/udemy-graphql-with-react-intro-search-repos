f41b0bf9c20496351bf4e838632f566784dfb2be
kawa-mon <kawashingo1990@gmail.com>
2022-05-09 13:05:57 +0900

    dynaic-search-form

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import React, { Component } from 'react'
 import { ApolloProvider } from 'react-apollo'
 import { Query } from 'react-apollo'
 import client from './client'
-import { ME } from './graphql'
 import { SEARCH_REPOSITORIES } from './graphql'
 
-const VARIABLES = {
+const DEFAULT_STATE = {
   first: 5,
   after: null,
   last: null,
@@ -16,15 +15,32 @@ const VARIABLES = {
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = VARIABLES
+    this.state = DEFAULT_STATE
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({
+      ...DEFAULT_STATE,
+      query: event.target.value,
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
   }
 
   render() {
     const { query, first, last, before, after } = this.state
+    console.log({ query })
+
     return (
       <ApolloProvider client={client}>
-        <div>Hello, GraphQL</div>
-
+        <form onSubmit={this.handleSubmit}>
+          <input value={query} onChange={this.handleChange} />
+        </form>
         <Query
           query={SEARCH_REPOSITORIES}
           variables={{ query, first, last, before, after }}
@@ -33,7 +49,7 @@ class App extends Component {
             if (loading) return 'Loading...'
             if (error) return `Error! ${error.message}`
 
-            console.log({data})
+            console.log({ data })
             return <div></div>
           }}
         </Query>
