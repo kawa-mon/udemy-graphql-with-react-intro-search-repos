0eabb6c70a06a249a38b540d890ac70129c309cc
kawa-mon <kawashingo1990@gmail.com>
2022-05-09 12:07:34 +0900

    App.js を講義と同じ形式に書き換え

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
+import React, { Component } from 'react'
 import { ApolloProvider } from 'react-apollo'
 import { Query } from 'react-apollo'
 import client from './client'
 import { ME } from './graphql'
 
-function App() {
-  return (
-    <ApolloProvider client={client}>
-      <div>Hello, GraphQL</div>
-      <Query query={ME}>
-        {({ loading, error, data }) => {
-          if (loading) return 'Loading...'
-          if (error) return `Error! ${error.message}`
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div>Hello, GraphQL</div>
 
-          return <div>{data.user.name}</div>
-        }}
-      </Query>
-    </ApolloProvider>
-  )
+        <Query query={ME}>
+          {({ loading, error, data }) => {
+            if (loading) return 'Loading...'
+            if (error) return `Error! ${error.message}`
+
+            return <div>{data.user.name}</div>
+          }}
+        </Query>
+      </ApolloProvider>
+    )
+  }
 }
 
 export default App
