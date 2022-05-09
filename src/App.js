669a065c0d72ad0430fc783de8e86280fabde07e
kawa-mon <kawashingo1990@gmail.com>
2022-05-09 16:55:40 +0900

    update totalCount with writeQuery

--- src/App.js
+++ src/App.js
@@ -14,6 +14,26 @@ const StarButton = (props) => {
         onClick={() =>
           addOrRemoveStar({
             variables: { input: { starrableId: node.id } },
+            update: (store, { data: { addStar, removeStar } }) => {
+              const { starrable } = addStar || removeStar
+              console.log(starrable)
+              const data = store.readQuery({
+                query: SEARCH_REPOSITORIES,
+                variables: { query, first, last, after, before },
+              })
+              const edges = data.search.edges
+              const newEdges = edges.map((edge) => {
+                if (edge.node.id === node.id) {
+                  const totalCount = edge.node.stargazers.totalCount
+                  const diff = starrable.viewerHasStarred ? 1 : -1
+                  const newTotalCount = totalCount + diff
+                  edge.node.stargazers.totalCount = newTotalCount
+                }
+                return edge
+              })
+              data.search.edges = newEdges
+              store.writeQuery({ query: SEARCH_REPOSITORIES, data })
+            },
           })
         }
       >
@@ -23,18 +43,7 @@ const StarButton = (props) => {
   }
 
   return (
-    <Mutation
-      mutation={viewerHasStarred ? REMOVE_STAR : ADD_STAR}
-      refetchQueries={(mutationResult) => {
-        console.log({ mutationResult })
-        return [
-          {
-            query: SEARCH_REPOSITORIES,
-            variables: { query, first, last, before, after },
-          },
-        ]
-      }}
-    >
+    <Mutation mutation={viewerHasStarred ? REMOVE_STAR : ADD_STAR}>
       {(addOrRemoveStar) => <StarStatus addOrRemoveStar={addOrRemoveStar} />}
     </Mutation>
   )
